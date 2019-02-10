a229999592a1445f3772e73f5e459ae54dd9704f
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2019-02-09 20:31:02 -0500

    Add files via upload

---
+++ Gun.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+    public ParticleSystem muscleflash;
+    public GameObject impactEffect;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+
+        }
+    }
+
+    void Shoot()
+    {
+        muscleflash.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+
+            Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+           
+        }
+    }
+
+    
+}
---
+++ UFOGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOGenerator : MonoBehaviour
+{
+    public GameObject UFOPrefab;
+
+
+    public int numberOfUFO = 1;
+    public float minimumx = -20f;
+    public float maximumx = 20f;
+    public float minimumz = -20f;
+    public float maximumz = 20f;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+
+        for (int i = 0; i < numberOfUFO; i++)
+        {
+            spawnPosition.z = Random.Range(minimumz, maximumz);
+            spawnPosition.y = 3;
+            spawnPosition.x = Random.Range(minimumx, maximumx);
+            Instantiate(UFOPrefab, spawnPosition, Quaternion.identity);
+
+        }
+    }
+}
